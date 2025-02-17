6e0e49ab3394b5fde52ca2c2d72d320ef197ca83
gustavolucenadev <gus_duarte8@hotmail.com>
2025-02-17 19:38:33 -0300

    Initial commit

---
+++ src/main/java/com/example/CRUD/projeto/ProjetoApplication.java
@@ -0,0 +1,18 @@
+package com.example.CRUD.projeto;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("TESTE!");
+	}
+}
